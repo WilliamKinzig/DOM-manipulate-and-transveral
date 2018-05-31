de4620a10fc8cbe896da5e53ec8ec7053bc3e2ca
William Kinzig <wippawill2@gmail.com>
2018-05-31 15:05:08 -0700

    finish DOM manipulation lesson

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
+
     $("ul#webpage").prepend("<li>Hello human!</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      (this).remove();
+    });
+
+    $("ul#webpage").children("li").first().click(function() {
+      (this).remove();
+
+    });
   });
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Peace-out!!</li>");
-    $("ul#webpage").prepend("<li>Peace out human!");
+    $("ul#webpage").prepend("<li>Peace out human!</li>");
   });
 
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>You stop copying me human!");
+    $("ul#webpage").prepend("<li>You stop copying me human!</li>");
   });
+
+
 });
